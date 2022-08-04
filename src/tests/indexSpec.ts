01ba300c29770ec5beb2c1408e64cbda6d68cd17
Nesren Samir <86182948+nesrensamir@users.noreply.github.com>
2022-08-04 14:46:25 +0200

    Update indexSpec.ts

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -20,4 +20,38 @@ const request = supertest(app);
       expect(response.status).toBe(200);
       
     });
+  }); 
+
+
+describe('test resize fn',()=>{
+    it('test resize', async()=>{
+      try {
+
+        const width = 111
+        const height = 400
+        const imagename = 'palmtunnel'
+      
+        await resize(
+           `../../images/full/${imagename}.jpg`,
+           width,
+           height,
+           `../../images/thumbnails/${imagename}${width}${height}.jpg`
+        )
+        expect(
+           fs.access(
+              `../../images/thumbnails/${imagename}${width}${height}.jpg`,
+              (Notexist) => {
+                 if (Notexist) {
+                    ;('Image Resized Successfully')
+                 }
+              }
+           )
+        )
+     } catch (err) {
+        return 'Image cannot be resized you need to check Image existance on the original folder & the values of width and height '
+     }
+
+    });
+
+
   });
